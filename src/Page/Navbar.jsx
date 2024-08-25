fea2278611f9ff153508b51d0c681f7659074ccf
Hiten <hitengamer11@gmail.com>
2024-08-25 22:31:38 +0545

    basic navbar template done

--- src/Page/Navbar.jsx
+++ src/Page/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='flex items-center justify-between bg-gray-900 text-white p-2'>
+            <div className='flex items-center justify-center gap-2 m-4'>
+                <h1>hide</h1>
+                <h1>Logo</h1>
+            </div>
+            <input type="text" placeholder='Search' className='rounded-3xl w-[30vw] text-white p-2 bg-gray-900 border-gray-500 border-2 placeholder:text-gray-400' />
+            <div className='flex items-center justify-center gap-2 mr-4'>
+                <h1>Not</h1>
+                <h1>Prof</h1>
+            </div>
+        </div>
+    )
 }
