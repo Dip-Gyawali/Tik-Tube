5cf1df670bcd6637c32eba918c39c0e2b5cc5e34
Hiten <hitengamer11@gmail.com>
2024-08-28 18:55:58 +0545

    more styling on sidebar

--- src/Page/Navbar.jsx
+++ src/Page/Navbar.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faBars} from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 import ProfilePic from '../assets/icons/profilePic.jpg'
 
 export default function Navbar() {
     return (
-        <div className='flex items-center justify-between bg-gray-900 text-white p-1'>
+        <div className='flex items-center justify-between bg-[#0f0f0f] text-white p-1 fixed top-0 z-10 w-[100vw]'>
             {/* youtube logos and side bar display button */}
             <div className='flex items-center justify-center gap-2 m-4'>
-                <FontAwesomeIcon icon={faBars} />
                 <FontAwesomeIcon icon={faYoutube} size="2x" color="red" />
                 <p className='text-[20px]'>YouTube</p>
             </div>
             {/* search bar */}
-            <div className='flex items-center justify-center gap-1 border-gray-500 border-2 rounded-[30px] px-3 py-2'>
-                <input type="text" placeholder='Search' className='border-none bg-transparent outline-none w-[20vw]' />
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            <div className='flex items-center justify-center gap-1 border-gray-500 border-2 rounded-[30px] overflow-hidden'>
+                <input type="text" placeholder='Search' className='border-none bg-transparent outline-none w-[20vw] px-3 py-2' />
+                <FontAwesomeIcon icon={faMagnifyingGlass} className='bg-[#ffffff14] p-4' />
             </div>
             {/* Profile logo and notification */}
             <div className='flex items-center justify-center gap-4 mr-4'>
--- src/Page/Sidebar.jsx
+++ src/Page/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faMusic, faGamepad, faFutbolBall, faTelevision, faMicrochip } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faMusic, faGamepad, faFutbolBall, faTelevision, faMicrochip, faFire, faGear, faFlag, faCircleQuestion, faMessage } from '@fortawesome/free-solid-svg-icons'
 import Hiten from '../assets/icons/hiten.jpg';
 import Ronaldo from '../assets/icons/urRonaldo.jpg';
 import Saket from '../assets/icons/saket.jpg';
@@ -12,40 +12,57 @@ import greatStack from '../assets/icons/great stack.jpg';
 
 export default function Sidebar() {
   return (
-    <div className='w-[10%] h-full bg-gray-900 text-white'>
-      {/* home button section */}
-      <div className='flex items-center gap-1'>
-        <FontAwesomeIcon icon={faHouse} />
-        <p>Home</p>
-      </div>
-      {/* music button section */}
-      <div className='flex items-center gap-1'>
-        <FontAwesomeIcon icon={faMusic} />
-        <p>Music</p>
-      </div>
-      {/* gaming button section */}
-      <div className='flex items-center gap-1'>
-        <FontAwesomeIcon icon={faGamepad} />
-        <p>Gaming</p>
-      </div>
-      {/* sports button section */}
-      <div className='flex items-center gap-1'>
-        <FontAwesomeIcon icon={faFutbolBall} />
-        <p>Sports</p>
-      </div>
-      {/* Entertainment button section */}
-      <div className='flex items-center gap-1'>
-        <FontAwesomeIcon icon={faTelevision} />
-        <p>Entertainment</p>
-      </div>
-      {/* Tech button section */}
-      <div className='flex items-center gap-1'>
-        <FontAwesomeIcon icon={faMicrochip} />
-        <p>Technology</p>
+    <div className='bg-[#0f0f0f] text-white fixed top-[72px] left-0 h-[93vh] w-[10%] z-10 overflow-y-auto custom-scrollbar'>
+      <div className=' p-3 flex flex-col gap-3'>
+
+        {/* home button section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faHouse} />
+          <p>Home</p>
+        </div>
+
+        {/* Trending Section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faFire} />
+          <p>Trending</p>
+        </div>
+
+        {/* music button section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faMusic} />
+          <p>Music</p>
+        </div>
+
+        {/* gaming button section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faGamepad} />
+          <p>Gaming</p>
+        </div>
+
+        {/* sports button section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faFutbolBall} />
+          <p>Sports</p>
+        </div>
+
+        {/* Entertainment button section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faTelevision} />
+          <p>Entertainment</p>
+        </div>
+
+        {/* Tech button section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faMicrochip} />
+          <p>Technology</p>
+        </div>
       </div>
       <hr />
+
+      {/* channels Subscribed */}
       <div className='p-3 flex flex-col gap-3'>
         <h4 className='text-[14px]'>Subscriptions</h4>
+
         {/* Hiten Channel */}
         <div className='flex gap-1'>
           <img src={Hiten} alt="hiten channel" className='h-[30px] w-[30px] rounded-[50%]' />
@@ -93,7 +110,40 @@ export default function Sidebar() {
           <img src={greatStack} alt="hiten channel" className='h-[30px] w-[30px] rounded-[50%]' />
           <p>GreatStack</p>
         </div>
+      </div>
+      <hr />
 
+      {/* setting and feedback section */}
+      <div className='p-3 flex flex-col gap-3'>
+
+        {/* setting section  */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faGear} />
+          <p>Setting</p>
+        </div>
+
+        {/* report problem section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faFlag} />
+          <p>Report</p>
+        </div>
+
+        {/* report problem section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faCircleQuestion} />
+          <p>Help</p>
+        </div>
+
+        {/* report problem section */}
+        <div className='flex items-center gap-1'>
+          <FontAwesomeIcon icon={faMessage} />
+          <p>Feedback</p>
+        </div>
+
+      </div>
+      <hr />
+      <div className='p-3 flex flex-col gap-3'>
+        <p>Terms Privacy Policy & Safety Copyright © 2024 Google LLC</p>
       </div>
     </div>
   )
