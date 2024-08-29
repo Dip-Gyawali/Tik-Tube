453dab2e46032ddbd87897fbc9ecfdda525606d3
Hiten <hitengamer11@gmail.com>
2024-08-29 13:39:47 +0545

    for you page design completed

--- src/Page/ForYouPage.jsx
+++ src/Page/ForYouPage.jsx
@@ -1,9 +1,116 @@
 import React from 'react'
+import thumbnail from '../assets/icons/thumbnail.jpeg'
 
 export default function ForYouPage() {
   return (
-    <div className=' bg-[#0f0f0f] text-white fixed right-0 top-[72px] z-10 overflow-y-auto h-[93vh] w-[90%]'>
-      <h1 className='text-[20vw]'>For You Page</h1>
+    <div className=' bg-[#0f0f0f] text-white fixed right-0 top-[72px] z-10 overflow-y-auto h-[93vh] w-[90%] grid grid-cols-4 p-8 g-5'>
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail" className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail" className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
+
+      <div className='p-5'>
+        <img src={thumbnail} alt="thumbnail"  className='border-none rounded-[20px]'/>
+        <h1>Title of the Video</h1>
+         <h3>Coding Channel</h3>
+         <p>1M views &bull; 2 days ago</p>
+      </div>
     </div>
   )
 }
